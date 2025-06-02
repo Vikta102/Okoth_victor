e54ac3f5dde50c316653b11903cb9a8535ad236a
Vikta102 <emmanuelyohana779@gmail.com>
2025-06-02 16:23:55 +0300

    change

--- lambda_function.py
+++
@@ -1,5 +0,0 @@
-# use lambda function to get square of list map [1,2,3,4,5]
-
-squares = list(map(lambda x: x**2, [1, 2, 3, 4, 5]))
-
-print(squares) 
--- return.py
+++
@@ -1,6 +0,0 @@
-# exercise
-# use return to divide two values using 
-
-def divide(a, b):
-    return a / b
-print(divide(10, 2))
